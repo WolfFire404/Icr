9420bad94821ae02afc7b7ba65f4b388a90cade3
WolfFire404 <reillyvas9@live.nl>
2017-09-19 13:44:46 +0200

    lol

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 velocity = Vector2.zero;
+    const float gravity = 20;
+    const float standardSpeed = 5;
+
+    void Move()
+    {
+
+        var pos = transform.position;
+        pos.x += velocity.x * Time.deltaTime;
+        pos.y += velocity.y * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    void Start()
+    {
+        velocity.x = standardSpeed;
+
+    }
+
+    void Update()
+    {
+        Move(); 
+    }
+}
